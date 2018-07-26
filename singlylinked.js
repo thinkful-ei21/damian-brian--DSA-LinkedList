b648f776d063a585cf4f675636502453ae2e7d5a
Damian Marius <damian.mm@live.com>
2018-07-26 12:27:38 -0700

    work on insert before

--- singlylinked.js
+++ singlylinked.js
@@ -5,7 +5,19 @@ const LinkedList = require('./linkedlist.js');
 function main(){
   const SLL = new LinkedList();
   
-  
+  function insertBefore(item, key){
+    // if head = key then replace this value
+    let currNode = this.head;
+    let previousNode = this.head;
+    while(this.head !== key){
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if(this.head === key){
+      this.insertFirst(item);
+    }
+}
+
   SLL.insertLast('Boomer');
   SLL.insertLast('Helo');
   SLL.insertLast('Husker');
@@ -13,11 +25,13 @@ function main(){
   SLL.insertLast('Tauhida');
   SLL.insertLast('Helo');
   SLL.insertLast('squirrel');
-  SLL.insertLast('Apollo');
+  SLL.insertLast('Apollo');  
+  insertBefore('Damian', 'Boomer');
   //SLL.remove('squirrel');
   console.log('runs');
   //console.log(SLL.find('squirrel'));
   console.log(JSON.stringify(SLL));
 }
+
 
 main();
